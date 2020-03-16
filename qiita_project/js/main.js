f5c721c3d9e9d05e872338511ee34e2e96b57b73
Accio26 <acc1iela@gmail.com>
2020-03-17 00:40:36 +0900

    HTML追記

--- qiita_project/js/main.js
+++ qiita_project/js/main.js
@@ -1,4 +1,3 @@
 // c47a08656c60d877ad2538578406f33ad66e2e93
 //https://l0lnp1f6p2.execute-api.ap-northeast-1.amazonaws.com/default/extend_qiita_api?token=c47a08656c60d877ad2538578406f33ad66e2e93&page=1&per_page=20
-
-Authorization: Bearer c47a08656c60d877ad2538578406f33ad66e2e93;
+// curl -X GET 'https://qiita.com/api/v2/authenticated_user/items?page=1&per_page=10' -H 'Authorization: Bearer c47a08656c60d877ad2538578406f33ad66e2e93'
